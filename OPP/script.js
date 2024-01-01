8d9239f0ea9294d393fe76bd4c309398d1d0a88a
wfpinedaf <wfpinedaf@outlook.com>
2024-01-01 18:55:30 -0500

    Map Doc - Map Marker

--- OPP/script.js
+++ OPP/script.js
@@ -17,6 +17,31 @@ if (navigator.geolocation)
         const { latitude } = position.coords
         const { longitude } = position.coords
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+        const coords = [latitude, longitude];
+
+        const map = L.map('map').setView(coords, 15);
+
+        L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+
+        map.on('click', function (mapEvent) {
+            console.log(mapEvent)
+            const { lat, lng } = mapEvent.latlng
+
+            L.marker([lat, lng]).addTo(map)
+                .bindPopup(L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup',
+
+                }))
+                .setPopupContent('workout')
+                .openPopup();
+        })
     }, function () {
         alert('Could not get your position')
     })
