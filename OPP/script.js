883888123114d18935bf11b80285f046fec4ff38
wfpinedaf <wfpinedaf@outlook.com>
2024-01-02 18:52:16 -0500

    clasess

--- OPP/script.js
+++ OPP/script.js
@@ -11,37 +11,80 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
 
-if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(function (position) {
+class App {
+    #map
+    #mapEvent
+    constructor() {
+        this._getPosition()
+
+        form.addEventListener('submit', this._newWorkout.bind(this))
+
+        inputType.addEventListener('change', function () {
+            inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+            inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+        })
+
+
+    }
+
+    _getPosition() {
+        if (navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+                alert('Could not get your position')
+            })
+
+    }
+
+    _loadMap(position) {
+
         const { latitude } = position.coords
         const { longitude } = position.coords
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
         const coords = [latitude, longitude];
 
-        const map = L.map('map').setView(coords, 15);
+        this.#map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(this.#map);
+
+        // Clicks on MAP
+        this.#map.on('click', this._showForm.bind(this))
+
+    }
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden')
+        inputDistance.focus()
+    }
+
+    _toggleElevationField() {
+
+    }
+    _newWorkout(e) {
+
+        e.preventDefault()
+        //clear Input fields
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ""
+
+        // Display Marker
+        const { lat, lng } = this.#mapEvent.latlng
+        L.marker([lat, lng]).addTo(this.#map).bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+        })).setPopupContent('workout')
+            .openPopup();
+
+    }
+}
+
+const app = new App();
 
 
-        map.on('click', function (mapEvent) {
-            console.log(mapEvent)
-            const { lat, lng } = mapEvent.latlng
 
-            L.marker([lat, lng]).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'running-popup',
 
-                }))
-                .setPopupContent('workout')
-                .openPopup();
-        })
-    }, function () {
-        alert('Could not get your position')
-    })
